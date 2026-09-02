e9426fdd60f1a28a706dafbcc5a647c95c4bffa1
ManoBigbig <manobigbig21@gmail.com>
2026-09-02 14:02:38 -0300

    Create minimal receiver

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,5 @@
 #include <switch.h>
+#include <cstdio>
 
 int main(int argc, char* argv[])
 {
@@ -6,16 +7,13 @@ int main(int argc, char* argv[])
 
     printf("WebVideoCasterNX\n");
     printf("================\n\n");
-    printf("Receiver iniciado.\n");
-    printf("Aguardando conexao...\n\n");
-    printf("Pressione + para sair.\n");
+    printf("Receiver iniciado!\n");
+    printf("Build 0.1 - teste inicial\n\n");
+    printf("O homebrew esta funcionando.\n");
 
     while (appletMainLoop())
     {
         consoleUpdate(NULL);
-
-        if (hidKeysDown(CONTROLLER_P1_AUTO) & KEY_PLUS)
-            break;
     }
 
     consoleExit(NULL);
