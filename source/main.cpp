a22e02c0be90d9899ef3d9bc3f96745e5398234d
ManoBigbig <manobigbig21@gmail.com>
2026-09-02 15:39:59 -0300

    minimal switch test

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,11 @@
 #include <switch.h>
-#include <cstdio>
 
 int main(int argc, char* argv[])
 {
-    consoleInit(NULL);
-
-    printf("WebVideoCasterNX\n");
-    printf("================\n\n");
-    printf("Receiver iniciado!\n");
-    printf("Build 0.1 - teste inicial\n\n");
-    printf("O homebrew esta funcionando.\n");
-
     while (appletMainLoop())
     {
-        consoleUpdate(NULL);
+        svcSleepThread(1000000000LL);
     }
 
-    consoleExit(NULL);
     return 0;
 }
